f15e53b8f05079daafda41663d797622a9dee5a5
Elric <29043797+elricdog@users.noreply.github.com>
2021-01-05 21:19:24 +0100

    Sistema perque a github els parametres debug per defecte siguin customitzables

---
+++ VRStarShip/env.js
@@ -0,0 +1,34 @@
+var url = window.location.href;
+console.log("Base URL: " + url);
+if (url.startsWith("https://127.0.0.1"))
+{
+	// Environment Configurations for development
+	console.log("Setting Environment Configurations for development");
+	_debugStats = true;
+	_debugShadow = false;
+	_debugDimensionGrids = true;
+	_debugCubeReference = true;
+	_debugHumanReference = false;
+	_debugDragonReference = true;
+	_debugLog = true;
+	_debugFixedCameras = true;
+
+	_controlsType = "map";
+	_hideHelpInitially = true;			
+}
+else
+{	
+	// Customized Environment Configurations for release
+	console.log("Setting Customized Environment Configurations for release");
+	_debugStats = false;
+	_debugShadow = false;
+	_debugDimensionGrids = false;
+	_debugCubeReference = false;
+	_debugHumanReference = true;
+	_debugDragonReference = true;
+	_debugLog = false;
+	_debugFixedCameras = false;
+
+	_controlsType = "orbit";
+	_hideHelpInitially = false;
+}
